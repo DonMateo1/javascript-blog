4b7d01484372bd75f75c712876357316e4cc66e2
Mateusz Windysz <mateuszwindysz@wp.pl>
2022-05-18 18:16:18 +0200

    usuniecie folderu node_modules

--- js/script.js
+++ js/script.js
@@ -42,7 +42,8 @@ function titleClickHandler(event) {
 
 const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+    optTitleListSelector = '.titles';,
+    optArticleTagSelector = '.post-tags .list';
 
 function generateTitleLinks() {
 
@@ -79,3 +80,31 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
+function generateTags(){
+    /* find all articles */
+  
+    /* START LOOP: for every article: */
+  
+      /* find tags wrapper */
+  
+      /* make html variable with empty string */
+  
+      /* get tags from data-tags attribute */
+  
+      /* split tags into array */
+  
+      /* START LOOP: for each tag */
+  
+        /* generate HTML of the link */
+  
+        /* add generated code to html variable */
+  
+      /* END LOOP: for each tag */
+  
+      /* insert HTML of all the links into the tags wrapper */
+  
+    /* END LOOP: for every article: */
+  }
+  
+  generateTags();
